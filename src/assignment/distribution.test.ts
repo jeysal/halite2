d1f008a34789278ce4ad396d9b4844a3b538fbce
Tim Seckinger <seckinger.tim@gmail.com>
2017-12-21 00:45:49 +0100

    fix(assignment): use heuristics to tame distribution algorithm complexity

--- src/assignment/distribution.test.ts
+++ src/assignment/distribution.test.ts
@@ -36,6 +36,23 @@ test('matches up the ships with their highest-value goals', () => {
   ).toMatchSnapshot();
 });
 
+test('matches up a single ship with its highest-value goal', () => {
+  const goals: Goal[] = [{ ...goalStub }, { ...goalStub }, { ...goalStub }];
+
+  expect(
+    distributeShips([
+      [
+        { goal: goals[0], shipId: 0, val: 0.1 },
+        { goal: goals[1], shipId: 0, val: 0.2 },
+        { goal: goals[2], shipId: 0, val: 0.7 },
+      ],
+    ] as ShipGoalValue[][]).map(({ goal, shipIds }) => ({
+      shipIds,
+      goalIndex: goals.indexOf(goal),
+    })),
+  ).toMatchSnapshot();
+});
+
 test('does not match a ship that has value zero everywhere', () => {
   const goals: Goal[] = [{ ...goalStub }, { ...goalStub }];
 
@@ -139,6 +156,27 @@ test('matches a ship to another goal to avoid undershooting minShips', () => {
         { goal: goals[2], shipId: 2, val: 0.7 },
       ],
     ] as ShipGoalValue[][]).map(({ goal, shipIds }) => ({
+      shipIds,
+      goalIndex: goals.indexOf(goal),
+    })),
+  ).toMatchSnapshot();
+});
+
+test('prefers an empty array over violating minShips if the heuristic fails', () => {
+  const goals: Goal[] = [
+    { ...goalStub, minShips: () => 12 },
+    { ...goalStub, minShips: () => 12 },
+    { ...goalStub, minShips: () => 12 },
+  ];
+
+  expect(
+    distributeShips(Array(15)
+      .fill(0)
+      .map((_, i) => [
+        { goal: goals[0], shipId: i, val: i % 3 === 0 ? 0.9 : 0.1 },
+        { goal: goals[1], shipId: i, val: i % 3 === 1 ? 0.9 : 0.1 },
+        { goal: goals[2], shipId: i, val: i % 3 === 2 ? 0.9 : 0.1 },
+      ]) as ShipGoalValue[][]).map(({ goal, shipIds }) => ({
       shipIds,
       goalIndex: goals.indexOf(goal),
     })),
--- src/assignment/distribution.ts
+++ src/assignment/distribution.ts
@@ -1,5 +1,5 @@
 import { OwnShipId } from 'halite/Ship';
-import { maxBy, reduce, sum, tail } from 'ramda';
+import { maxBy, reduce, sortBy, sum, tail } from 'ramda';
 
 import Goal from '../goal/goal';
 
@@ -10,9 +10,27 @@ interface Distribution<Turn = number> {
   totalValue: number;
 }
 
+/**
+ * Recursively calculate the best distribution.
+ * Computational complexity is exponential (n^m with shipGoalValues of size [m][n]),
+ * but both m and n can be limited by applying heuristics, controlled by the optional parameters.
+ * Note that the worst case of this heuristic is an empty array in spite of the existence of a distribution with positive total value.
+ * It may be useful to check the return value for an empty array and retry with different limits.
+ * @param recursionBranchLimit How many of the heuristically best goals to recurse into per ship.
+ *                             A higher value results in a potentially better distribution but also increases computation time.
+ *                             This limits n (the base) of algorithmic complexity.
+ * @param recursionDepthLimit How far from the last ship to start branching.
+ *                            Only after this point, recursionBranchLimit will actually have any effect.
+ *                            Prior to this point, recursionBranchLimit is effectively 1.
+ *                            This limits m (the exponent) of algorithmic complexity.
+ *                            For a meaningful heuristic, shipGoalValues should be sorted
+ *                            (e.g. moving ships with a single prominently high goal value to the beginning of the array).
+ */
 const recurseValues = <Turn>(
   { goalsWithShips, totalValue }: Distribution<Turn>,
   shipGoalValues: ShipGoalValue<Turn>[][],
+  recursionBranchLimit: number,
+  recursionDepthLimit: number,
 ): Distribution<Turn> => {
   // fail if goals have been picked too broadly
   const missingShips = sum(
@@ -37,9 +55,24 @@ const recurseValues = <Turn>(
           distributedGoal.maxShips() > shipIds.length,
       ),
   );
+  // ignore this ship, continue with the next
+  if (!usableGoalValues.length)
+    return recurseValues(
+      { goalsWithShips, totalValue },
+      tail(shipGoalValues),
+      recursionBranchLimit,
+      recursionDepthLimit,
+    );
+
+  const heuristicallyBestGoalValues = sortBy(
+    ({ val }) => val,
+    usableGoalValues,
+  ).slice(
+    -(shipGoalValues.length <= recursionDepthLimit ? recursionBranchLimit : 1),
+  );
 
   // recursively calculated best distribution for each usable goal
-  const completeGoalValues = usableGoalValues.map(
+  const completeGoalValues = heuristicallyBestGoalValues.map(
     ({
       goal: augmentedGoal,
       shipId: augmentingShipId,
@@ -66,13 +99,15 @@ const recurseValues = <Turn>(
           totalValue: totalValue + augmentationValue,
         },
         tail(shipGoalValues),
+        recursionBranchLimit,
+        recursionDepthLimit,
       );
     },
   );
 
   return reduce(
     maxBy(({ totalValue }) => totalValue),
-    { goalsWithShips, totalValue },
+    { goalsWithShips: [], totalValue: 0 },
     completeGoalValues,
   );
 };
@@ -89,7 +124,18 @@ export interface GoalWithShips<Turn = number> {
 const distributeShips = <Turn>(
   shipGoalValues: ShipGoalValue<Turn>[][],
 ): GoalWithShips<Turn>[] => {
-  return recurseValues({ goalsWithShips: [], totalValue: 0 }, shipGoalValues)
-    .goalsWithShips;
+  const shipGoalValuesByTopGoalProminenceDesc = sortBy(goalValues => {
+    const sortedGoalValues = sortBy(({ val }) => -val, goalValues);
+    return sortedGoalValues[1].val - sortedGoalValues[0].val; // descending prominence
+  }, shipGoalValues);
+
+  const { goalsWithShips } = recurseValues(
+    { goalsWithShips: [], totalValue: 0 },
+    shipGoalValuesByTopGoalProminenceDesc,
+    3,
+    8,
+  );
+
+  return goalsWithShips;
 };
 export default distributeShips;
